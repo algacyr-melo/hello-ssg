beaaf59794fe984c62c8fe0984e57d7604071382
Algacyr Melo <algacyr.melo@gmail.com>
2025-04-11 21:30:19 -0300

    Extract images and links from raw markdown

--- src/main.py
+++ src/main.py
@@ -3,17 +3,12 @@ from textnode import TextNode, TextType
 
 
 def main():
-    html_node = HTMLNode(
-        "a",
-        "This is a Link",
-        [],
-        {
-            "href": "https://www.google.com",
-            "target": "_blank",
-        }
-    )
-    print(repr(html_node))
 
+    text = """
+This is text with a link [to boot dev](https://www.boot.dev)
+and [to youtube](https://www.youtube.com/@bootdotdev)
+"""
+    print(text)
 
 main()
 
