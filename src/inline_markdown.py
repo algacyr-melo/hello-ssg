b8bfc5dfe584d57f77b408f9b4f1c19fe85befbf
Algacyr Melo <alsdmelo@protonmail.com>
2025-04-24 22:51:48 -0300

    Split image nodes passed first test

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -74,3 +74,53 @@ def split_nodes_delimiter_iterative(old_nodes, delimiter, text_type):
         new_nodes.extend(split_nodes)
     return new_nodes
 
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for old_node in old_nodes:
+        matches = extract_markdown_images(old_node.text)
+
+        # Did we extract any images?
+        if len(matches) == 0:
+            new_nodes.append(old_node)
+            continue
+
+        original_text = old_node.text
+        for image_alt, image_url in matches:
+            split_nodes = []
+
+            sections = original_text.split(f"![{image_alt}]({image_url})", 1)
+
+            if len(sections[0]) > 0:
+                split_nodes.append(TextNode(sections[0], TextType.TEXT))
+            split_nodes.append(TextNode(image_alt, TextType.IMAGE, image_url))
+            new_nodes.extend(split_nodes)
+
+            original_text = sections[-1]
+        return new_nodes
+
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for old_node in old_nodes:
+        matches = extract_markdown_links(old_node.text)
+
+        # Did we extract any links?
+        if len(matches) == 0:
+            new_nodes.append(old_node)
+            continue
+
+        original_text = old_node.text
+        for link_text, link_url in matches:
+            split_nodes = []
+
+            sections = original_text.split(f"[{link_text}]({link_url})", 1)
+
+            if len(sections[0]) > 0:
+                split_nodes.append(TextNode(sections[0], TextType.TEXT))
+            split_nodes.append(TextNode(link_text, TextType.LINK, link_url))
+            new_nodes.extend(split_nodes)
+
+            original_text = sections[-1]
+        return new_nodes
+
